a9ea7d45de199af422da7e834dc706b747692110
Jorgeluisreis <ctt.jluis@gmail.com>
2025-03-22 23:05:15 -0300

    init: Deploy inicial.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Digite um número para verificar ele pertence a uma sequência de Fibonacci: ");
+if (int.TryParse(Console.ReadLine(), out int numero))
+{
+    if (PertenceFibonacci(numero))
+        Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+    else
+        Console.WriteLine($"O número {numero} NÃO pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Digite um número inteiro para validar..");
+}
+
+static bool PertenceFibonacci(int n)
+{
+    if (n < 0) return false;
+
+    int a = 0, b = 1;
+    while (a <= n)
+    {
+        if (a == n) return true;
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return false;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# TargetFibonacci
+
+2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), usando C#, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
